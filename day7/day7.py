3ed6fc1a4b37021c3c2aaba6cd25c4b8d9bb3c45
Carl Harris <elgoogemail2007@gmail.com>
2016-04-16 02:51:11 -0400

    TRANSFER: another shot and a miss

--- day7/day7.py
+++ day7/day7.py
@@ -1,5 +1,5 @@
 #!python2
-import operator
+from operator import itemgetter
 from pprint import pprint as pp
 
 simple = """\
@@ -45,51 +45,127 @@ OPS = {
     'NOT': NOT,
 }
 
+def makecircuit(lines):
+    circuit = {}
+    for line in lines:
+        line = line.strip()
+        lhs, endpoint = line.split(' -> ')
+        result = get_op(lhs)
+        if result:
+            name, op = result
+            i1, i2 = map(str.strip, lhs.split(name))
+            args = tuple(safeint(i) for i in [i1, i2] if i )
+            circuit[op] = args
+            circuit[endpoint] = op
+        else:
+            circuit[endpoint] = safeint(lhs)
+    return circuit
+
 def get_op(s):
     for k,v in OPS.items():
         if k in s:
             return k,v()
 
-def is_op(thing):
-    return any(isinstance(thing, cls) for cls in OPS.values())
+def isop(thing):
+    return isinstance(thing, tuple(OPS.values()))
 
-def process(circuit, op=None, args=None):
+def isint(thing):
+    return isinstance(thing, int)
 
-    for k,v in circuit.items():
-        if is_op(k):
-            #circuit[endpoint] = k(*process(args))
-            return process(circuit, k, v)
+def safeint(thing):
+    try:
+        return int(thing)
+    except ValueError:
+        return thing
+
+def is_sequence(thing):
+    try:
+        iter(thing)
+    except ValueError:
+        return False
+    else:
+        return True
+
+def resolve2(circuit, *keys):
+    print 'resolve:keys: %s' % (keys, )
+
+    if len(keys) == 0:
+        return []
+
+    vals = []
+    for key in keys:
+        try:
+            val = circuit[key]
+        except KeyError:
+            vals = [ keys[0], ] + resolve(circuit, *keys[1:])
+            break
+
+        print 'resolve:key:%s, val:%s' % (key, val)
+
+        if isint(val):
+            vals.append(val)
         else:
-            return (k,v)
+            if isop(key):
+                op = key
+                args = resolve(circuit, *val)
+                print '%s(%s)' % (op, args)
+                return op(*args)
+            else:
+                return resolve(circuit, *val)
 
-def resolve(circuit, *args):
-    if all(arg for arg in args if not is_op(arg)):
-        getter = operator.itemgetter(*args)
-        return getter(circuit)
-    resolve(circuit, *[ resolve(circuit, arg) if is_op(arg) else arg for arg in args ])
+    print 'resolve:vals:%s' % (vals, )
+    return vals
 
-def main():
-    circuit = {}
+def resolve(circuit, *keys):
+    print
+    print 'keys: %s' % (keys, )
 
-    for line in simple.split('\n'):
-        lhs, endpoint = line.split(' -> ')
+    getter = itemgetter(*keys)
+    values = getter(circuit)
+    print 'values: %s' % (values, )
+    print 'type(values) == %s' % (type(values), )
 
-        result = get_op(lhs)
-        if result:
-            name, op = result
-            i1, i2 = lhs.split(name)
-            i1 = i1.strip()
-            i2 = i2.strip()
-            args = tuple(i for i in [endpoint, i1, i2] if i )
-            circuit[op] = args
-        else:
-            circuit[endpoint] = int(lhs)
+    if not isinstance(values, tuple):
+        values = (values, )
 
+    values = tuple(v if isint(v) else resolve(circuit, v) for v in values)
+
+    return resolve(circuit, *values)
+
+def find_path(graph, start, end, path=[]):
+    path = path + [start]
+    if start == end:
+        return path
+    if not graph.has_key(start):
+        return None
+    for node in graph[start]:
+        if node not in path:
+            newpath = find_path(graph, node, end, path)
+            if newpath:
+                return newpath
+    return None
+
+def main_simple():
+    circuit = makecircuit(simple.splitlines())
+
+    print 'circuit:'
     pp(circuit)
 
-    for k in circuit.keys():
-        print 'resolving %s' % k
-        print resolve(circuit, k)
+    key = 'd'
+    print 'resolve(circuit, %s)' % key
+    print 'result: %s' % resolve(circuit, key)
+
+def main_real():
+    with open('input') as f:
+        circuit = makecircuit(f.readlines())
+
+        pp(circuit)
+
+        print resolve(circuit, 'a')
+
+def main():
+    #main_simple()
+    main_real()
 
 if __name__ == '__main__':
     main()
